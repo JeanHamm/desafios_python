67ed4209de8b38e2e8f2abf71347881944b6dbbb
Jean Hamm <jeanmichehamm@gmail.com>
2023-08-02 23:34:49 -0300

    Stone 02/08/23
    Criação do jogo jogkenpo

---
+++ jokenpo/jokenpo.py
@@ -0,0 +1,17 @@
+class Jogador:
+    def __init__(self, nome, jogada):
+        self.nome = nome
+        self.jogada = jogada
+        pass
+def well_come(mensagem):
+    print('-------------------')
+    print(mensagem)
+    print('-------------------')
+
+
+
+well_come('Wellcome to jokenpo')
+
+player = Jogador('player', )
+
+
